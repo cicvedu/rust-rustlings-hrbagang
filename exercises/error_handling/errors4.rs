d4ebcb1f1dccee3208891ff5d299e1793b14729d
unknown <PES@gang-xiao.yidatec.com>
2023-08-19 19:58:56 +0800

    anything

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -16,7 +16,14 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
+        if value < 1 {
+            if value == 0 {
+                return Err(CreationError::Zero);
+            } else {
+                return Err(CreationError::Negative);
+            }
+        }
+
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }
@@ -30,3 +37,4 @@ fn test_creation() {
     );
     assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
 }
+
--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[cfg(test)]
 mod tests {
@@ -12,8 +12,8 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        // Use if let to extract the value from the Some variant
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -29,10 +29,8 @@ mod tests {
 
         let mut cursor = range;
 
-        // TODO: make this a while let statement - remember that vector.pop also
-        // adds another layer of Option<T>. You can stack `Option<T>`s into
-        // while let and if let.
-        integer = optional_integers.pop() {
+        // Use while let to iterate over the vector and unwrap the nested Option
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
@@ -40,3 +38,4 @@ mod tests {
         assert_eq!(cursor, 0);
     }
 }
+
